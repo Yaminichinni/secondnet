b80099035d96db65de7570dacdcd14cdfdc0b44e
Yaminichinni <86146436+Yaminichinni@users.noreply.github.com>
2021-06-19 15:42:40 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+
+namespace AsncAww1
+{
+    class Program
+    {
+
+
+
+
+        public static async Task Method1()
+        {
+            await Task.Run(() =>
+            {
+                string a = Method2();
+                Console.WriteLine(a);
+
+
+
+            });
+        }
+
+
+
+
+        public static string Method2()
+        {
+            Console.WriteLine("wait for string to return");
+            Thread.Sleep(5000);
+            return "this is method 2";
+        }
+        static void Main(string[] args)
+        {
+            Method1();
+
+            Console.ReadKey();
+        }
+
+
+
+    }
+}
